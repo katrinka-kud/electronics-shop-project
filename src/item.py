aa86adc63d7a07304317b3676bdac113eae59039
Екатерина Кудряшова <katyushka555@yandex.ru>
2024-01-15 14:00:22 +0400

    homework2 доработка

--- src/item.py
+++ src/item.py
@@ -22,16 +22,23 @@ class Item:
 
         self.all.append(self)
 
+
+    def __repr__(self) -> str:
+        return f'Имя: {self.name}, цена: {self.price}'
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
     @property
-    def name(self):
+    def name(self) -> str:
         return self.__name
 
     @name.setter
     def name(self, name):
-        if len(name) <= 10:
-            self.__name = name
+        if len(name) > 10:
+            raise Exception('Длина наименования товара превышает 10 символов.')
         else:
-            raise (Exception('Длина наименования товара превышает 10 символов.'))
+            self.__name = name
 
     def calculate_total_price(self) -> float:
         """
@@ -45,31 +52,26 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price = self.price * Item.pay_rate
-        return self.price
+        self.price *= self.pay_rate
+
+    @staticmethod
+    def string_to_number(digit: str) -> int:
+        return int(float(digit))
 
     @classmethod
-    def instantiate_from_csv(cls):
+    def instantiate_from_csv(cls, filename='../src/items.csv') -> None:
         """
         Загружает данные из csv файла и преобразует их в список словарей
         """
-        cls.all = []
-        with open('items.csv', 'r', encoding='UTF-8', newline='') as f:
+        cls.all.clear()
+
+        with open(filename, 'r', encoding='UTF-8', newline='') as f:
             reader = csv.DictReader(f)
             for line in reader:
-                cls.all.append(line)
-            return cls.all
-
-    @staticmethod
-    def string_to_number(digit):
-        is_int = float(digit).is_integer()
-        return is_int
-
-    def __repr__(self) -> str:
-        return f'Имя: {self.name}, цена: {self.price}'
-
-    def __str__(self) -> str:
-        return f'{self.name}'
+                name = line['name']
+                price = float(line['price'])
+                quantity = int(line['quantity'])
+                cls(name, price, quantity)
 
 
 class Phone(Item):
